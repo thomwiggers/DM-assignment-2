3b9964adc28ec1c761384f9d7b378d4b607b0802
Thom Wiggers <thom@thomwiggers.nl>
2014-03-10 13:03:33 +0100

    Added correlations

--- Python Scripts/assignment.py
+++ Python Scripts/assignment.py
@@ -6,8 +6,8 @@ Data mining assignment 2
 @author: Thom Wiggers (s4119444)
 """
 import scipy.io
+import scipy.stats
 import pylab
-import numpy as np
 
 data = scipy.io.loadmat('../Data/wine.mat')
 
@@ -18,19 +18,19 @@ data = scipy.io.loadmat('../Data/wine.mat')
 #  column 8 density (1)
 #  column 11 (alcohol) (5-20)
 
-matrix = np.asmatrix(data['X'])
+matrix = pylab.np.asmatrix(data['X'])
 
 #  volumetric acidity
 pylab.boxplot(matrix[:,1])
 pylab.title('Volumetric acidity boxplot (not corrected)')
 pylab.ylabel('gdm^-3')
-pylab.show()
+#pylab.show()
 
 pylab.hist(matrix[:,1])
 pylab.title('histogram volumetric acidity (not corrected)')
 pylab.ylabel('frequency')
 pylab.xlabel('volumetric acidity (gdm^-3)')
-pylab.show()
+#pylab.show()
 
 #  remove outliers
 acidity_no_outliers = matrix[:,1][matrix[:,1]<20]
@@ -38,26 +38,26 @@ acidity_no_outliers = matrix[:,1][matrix[:,1]<20]
 pylab.boxplot(acidity_no_outliers)
 pylab.title('volumetric acidity boxplot (outliers removed)')
 pylab.ylabel('gdm^-3')
-pylab.show()
+#pylab.show()
 
 pylab.hist(acidity_no_outliers.T) #transpose for some weird reason.
 pylab.title('Volumetric acidity histogram (outliers removed)')
 pylab.xlabel('Volumetric acidity (gdm^-3)')
 pylab.ylabel('frequency')
-pylab.show()
+#pylab.show()
 
 #  density
 pylab.boxplot(matrix[:,7])
 pylab.title('Density (not corrected)')
 pylab.ylabel('Density (gcm^-3)')
-pylab.show()
+#pylab.show()
 
 #  density histogram
 pylab.hist(matrix[:,7])
 pylab.title('histogram density (not corrected)')
 pylab.xlabel('Density (gcm^-3)')
 pylab.ylabel('frequency')
-pylab.show()
+#pylab.show()
 
 #  remove outliers for density
 density_no_outliers = matrix[:,7][matrix[:,7]<1]
@@ -66,14 +66,14 @@ density_no_outliers = matrix[:,7][matrix[:,7]<1]
 pylab.boxplot(density_no_outliers)
 pylab.title('Density (outliers removed)')
 pylab.ylabel('Density (gcm^-3)')
-pylab.show()
+#pylab.show()
 
 #  density histogram
 pylab.hist(density_no_outliers.T)
 pylab.title('histogram density (outliers removed)')
 pylab.xlabel('Density (gcm^-3)')
 pylab.ylabel('frequency')
-pylab.show()
+#pylab.show()
 
 
 #
@@ -83,14 +83,14 @@ pylab.show()
 pylab.boxplot(matrix[:,10])
 pylab.title('alcohol (not corrected)')
 pylab.ylabel('alcohol (% vol)')
-pylab.show()
+#pylab.show()
 
 #  alcohol histogram
 pylab.hist(matrix[:,10])
 pylab.title('histogram alcohol (not corrected)')
 pylab.xlabel('alcohol (% vol)')
 pylab.ylabel('frequency')
-pylab.show()
+#pylab.show()
 
 #remove outliers
 alcohol_no_outliers = matrix[:,10][matrix[:,10]<200]
@@ -99,19 +99,53 @@ alcohol_no_outliers = matrix[:,10][matrix[:,10]<200]
 pylab.boxplot(alcohol_no_outliers)
 pylab.title('alcohol (outliers removed)')
 pylab.ylabel('alcohol (% vol)')
-pylab.show()
+#pylab.show()
 
 #  alcohol histogram
 pylab.hist(alcohol_no_outliers.T)
 pylab.title('histogram alcohol (outliers removed)')
 pylab.xlabel('alcohol (% vol)')
 pylab.ylabel('frequency')
-pylab.show()
-
+#pylab.show()
+pylab.close()
 
 # TODO Add comparisons between corrected and non-correcte data.
 
-
 ## 2.1.2
 
+attributes = [
+    {'name': 'fixed acidity',
+     'unit': 'gdm^-3'},
+    {'name': 'volatile acidity',
+     'unit': 'gdm^-3'},
+    {'name': 'citric acid',
+     'unit': 'gdm^-3'},
+    {'name': 'residual sugar',
+     'unit': 'gdm^-3'},
+    {'name': 'chlorides',
+     'unit': 'gdm^-3'},
+    {'name': 'free sulfur dioxides',
+     'unit': 'mg.dm^-3'},
+    {'name': 'total sulfur dioxides',
+     'unit': 'mg.dm^-3'},
+    {'name': 'density',
+     'unit': 'gdm^-3'},
+    {'name': 'pH',
+     'unit': 'pH'},
+    {'name': 'sulphates',
+     'unit': 'gdm^-3'},
+    {'name': 'alcohol',
+     'unit': '% vol'},
+]
+x = pylab.np.asarray(matrix[:,11])
+for n in xrange(10):
+  y = pylab.np.asarray(matrix[:,n])
+  pylab.scatter(x, y)
+  pylab.title("Scatter plot: %s and score" % attributes[n]['name'])
+  pylab.xlabel("score (points)")
+  pylab.ylabel("%s (%s)" % (attributes[n]['name'], attributes[n]['unit']))
+  pylab.show()
 
+  print "correlation between %s and score: %1.3f" % (
+      attributes[n]['name'], scipy.stats.pearsonr(x,y)[0][0])
+  
